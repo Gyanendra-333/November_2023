dbaddf980d48003def8a5766d06ed61a2e34d75a
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-11-04 08:54:24 +0530

    Array insert

---
+++ Array/insertion.js
@@ -0,0 +1,14 @@
+
+let data = [12, 3, 4, 57, 8, 9];
+let newEl = 10;
+let position = 3;
+
+for (let i = data.length - 1; i >= 0; i--) {
+    if (i >= position) {
+        data[i + 1] = data[i];
+        if (i == position) {
+            data[i] = newEl;
+        }
+    }
+}
+console.log(data);
