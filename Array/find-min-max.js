70bd96ab6ddfce30809245a048bdb6f7115d13ad
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-11-06 17:22:09 +0530

    Find Min Max

---
+++ Array/find-min-max.js
@@ -0,0 +1,23 @@
+// JavaScript code for the approach
+
+// Function to find minimum and maximum values in an array
+function findMinMax(arr) {
+    let min = arr[0];
+    let max = arr[0];
+
+    // Iterate through the array and compare each element with current min and max
+    for (let i = 1; i < arr.length; i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        } else if (arr[i] > max) {
+            max = arr[i];
+        }
+    }
+
+    return { min, max };
+}
+
+// Example usage
+const arr = [1, 423, 6, 46, 34, 23, 13, 53, 4];
+const { min, max } = findMinMax(arr);
+console.log(min - ${ min } max - ${ max }); // Output: min-1 max-423
