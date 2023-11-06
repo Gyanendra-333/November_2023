a447fa9c6c6213d60466ac78a6c631c07fe1cf0e
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-11-06 17:22:54 +0530

    Find Min Max

--- Array/find-min-max.js
+++ Array/find-min-max.js
@@ -17,7 +17,6 @@ function findMinMax(arr) {
     return { min, max };
 }
 
-// Example usage
 const arr = [1, 423, 6, 46, 34, 23, 13, 53, 4];
 const { min, max } = findMinMax(arr);
 console.log(min - ${ min } max - ${ max }); // Output: min-1 max-423
