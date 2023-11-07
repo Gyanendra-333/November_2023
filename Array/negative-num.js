ba42879521bd7a3d258826d52a0fc6d323649dd1
Gyanendra Prakash <pgyanendra333@gmail.com>
2023-11-07 20:45:15 +0530

    Check Negative Number

--- Array/negative-num.js
+++ Array/negative-num.js
@@ -1,18 +1,18 @@
-<script>
+
 
 // JavaScript Code for the same approach
 
-    function move(arr){
-        arr.sort(); 
+function move(arr) {
+    arr.sort();
 }
 
-    // driver code 
+// driver code 
 
-    let arr = [ -1, 2, -3, 4, 5, 6, -7, 8, 9 ];
-    move(arr);
-    for (let e of arr)
-    document.write(e , " ");
+let arr = [-1, 2, -3, 4, 5, 6, -7, 8, 9];
+move(arr);
+for (let e of arr)
+    document.write(e, " ");
 
 // This code is contributed by shinjanpatra 
 
-</script>
+
